020e34ce4c35c81a4cfc536000794ac68d6f932b
shadowwwwww <2387749377@qq.com>
2024-07-24 16:13:19 +0800

    名字

--- src/locales/zh-CN.ts
+++ src/locales/zh-CN.ts
@@ -102,8 +102,8 @@ export default {
     small: '小'
   },
   login: {
-    welcome: '欢迎使用本系统',
-    message: '日志查看系统',
+    welcome: '欢迎使用本平台',
+    message: '链上混淆数据检索权限强制性访问控制平台',
     username: '用户名',
     password: '密码',
     register: '注册',
